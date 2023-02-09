cf4c39f444f5ba4fb260bb85c6d9eeb940072a19
Đoàn Nam <100189938+namy1622@users.noreply.github.com>
2023-02-09 23:29:03 +0700

    Add files via upload

---
+++ api1.java
@@ -0,0 +1,16 @@
+package classss;
+
+
+	import java.util.Scanner;
+
+	class api1
+	{
+	  public static void main(String[] args) {
+	    Scanner myObj = new Scanner(System.in);
+	    System.out.println("Enter username");
+
+	    String userName = myObj.nextLine();
+	    System.out.println("Username is: " + userName);
+	  }
+	}
+
